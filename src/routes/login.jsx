c09d643137b998e41f4b2c1db68a3b632c69696b
shibilipp007 <shibilishareef007@gmail.com>
2024-10-08 14:40:30 +0530

    Error message added

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -37,6 +37,7 @@ export default function Login() {
           id={"email"}
           type={"email"}
           {...register("email", { required: true })}
+          error={errors.email?.message}
         />
 
         <Input
@@ -44,6 +45,7 @@ export default function Login() {
           id={"password"}
           type={"password"}
           {...register("password", { required: true })}
+          error={errors.password?.message}
         />
         <button
           className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-full"
